e1441f67084f11346d1434d58edba98cfb4126e3
Matt Haffner <mrhaffner13@gmail.com>
2020-12-01 14:24:28 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import NavBar from './components/NavBar';
+import BlogsPage from './components/BlogsPage';
+import LogIn from './components/LogIn';
+import PostPage from './components/PostPage';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const hyphenate = (str) => {
+      return str.toLowerCase().split(' ').join('-')
+    }
+  
+    const addHyphenTitle = (arr) => {
+      arr.forEach((obj) => {
+        obj.hyphenTitle = hyphenate(obj.title)
+      })
+    }
+    const getPosts = async () => {
+      try {
+        //set isLoading to true?
+        const response = await fetch('http://localhost:3000/blog', {method: 'GET', mode: 'cors'})
+        const data = await response.json()
+        addHyphenTitle(data)
+        setPosts(data.reverse())
+        setIsLoading(false)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getPosts()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path='/blog/:hyphenTitle' 
+            render={() => {
+              return ( isLoading ? <h2>Loading Page</h2> : <PostPage posts={posts} />)
+            }}>
+          </Route>
+          <Route path='/blog'>
+            <BlogsPage posts={posts} isLoading={isLoading} />
+          </Route>
+          <Route path='/'>
+            <LogIn />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
